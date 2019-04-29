573a11df89d39792bc35c02e989b4be68670bcb0
David Witter <david.witter@wisc.edu>
2019-04-29 13:27:57 -0500

    tweak subheader and add divider

--- src/myuw-drawer-subheader.js
+++ src/myuw-drawer-subheader.js
@@ -3,13 +3,52 @@ import tpl from './myuw-drawer-subheader.html';
 export class MyUWDrawerSubheader extends HTMLElement {
   constructor() {
     super();
-    console.log('built')
 
     // Create a shadow-root for this element.
     this.attachShadow({ mode: 'open' });
 
     // Append the custom HTML template to the shadow-root.
     this.shadowRoot.appendChild(MyUWDrawerSubheader.template.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return [
+      'name',
+      'divider'
+    ];
+  }
+
+  connectedCallback() {
+    // Get attributes and assign elements
+    this['name']    = this.getAttribute('name') || '';
+    this['divider'] = this.getAttribute('divider') == 'true' ? true : false;
+
+    this.$label   = this.shadowRoot.getElementById('label');
+    this.$divider = this.shadowRoot.getElementById('divider');
+
+    if (this.$label != null 
+      && this['name'].length > 0) {
+      this.updateComponent();
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // Update the attribute internally
+    this[name] = newValue;
+    // Update the component
+    if (this.$label) {
+      this.updateComponent();
+    }
+  }
+
+  updateComponent() {
+    // Set subheader label
+    this.$label.innerHTML = `<span>${this['name']}</span>`;
+
+    // Display divider
+    if (this['divider'] === true) {
+      this.$divider.style.display = 'block';
+    }
   }
 }
 
